f49553dedbbba068e7aa87590d4539bd90664310
bibarub <wi1717171713@gmail.com>
2023-07-29 15:02:51 +0000

    vayu: Downgrade fingerprint in order to make SafetyNet pass
    * Also remove "vayu_eea" config from libinit since it doesn't exist

--- libinit/init_xiaomi_vayu.cpp
+++ libinit/init_xiaomi_vayu.cpp
@@ -9,7 +9,7 @@
 
 #include "vendor_init.h"
 
-static const variant_info_t vayu_global_info = {
+static const variant_info_t vayu_info = {
     .hwc_value = "GLOBAL",
     .sku_value = "",
 
@@ -18,21 +18,7 @@ static const variant_info_t vayu_global_info = {
     .mod_device = "vayu_global",
     .marketname = "POCO X3 Pro",
     .model = "M2102J20SG",
-    .build_fingerprint = "POCO/vayu_global/vayu:13/RKQ1.200826.002/V14.0.1.0.TJUMIXM:user/release-keys",
-
-    .nfc = true,
-};
-
-static const variant_info_t vayu_eea_info = {
-    .hwc_value = "EEA",
-    .sku_value = "",
-
-    .brand = "POCO",
-    .device = "vayu",
-    .mod_device = "vayu_eea_global",
-    .marketname = "POCO X3 Pro",
-    .model = "M2102J20SG",
-    .build_fingerprint = "POCO/vayu_eea/vayu:12/RKQ1.200826.002/V13.0.6.0.SJUEUXM:user/release-keys",
+    .build_fingerprint = "POCO/vayu_global/vayu:11/RKQ1.200826.002/V12.0.4.0.RJUMIXM:user/release-keys",
 
     .nfc = true,
 };
@@ -43,21 +29,22 @@ static const variant_info_t bhima_info = {
 
     .brand = "POCO",
     .device = "bhima",
-    .mod_device = "vayu_in_global",
+    .mod_device = "bhima_in",
     .marketname = "POCO X3 Pro",
     .model = "M2102J20SI",
-    .build_fingerprint = "POCO/bhima_in/bhima:12/RKQ1.200826.002/V13.0.5.0.SJUINXM:user/release-keys",
+    .build_fingerprint = "POCO/bhima_in/bhima:11/RKQ1.200826.002/V12.0.4.0.RJUINXM:user/release-keys",
 
     .nfc = false,
 };
 
 static const std::vector<variant_info_t> variants = {
-    vayu_global_info,
-    vayu_eea_info,
+    vayu_info,
     bhima_info,
 };
 
 void vendor_load_properties() {
     set_dalvik_heap();
+    #ifndef __ANDROID_RECOVERY__
     search_variant(variants);
+    #endif
 }
